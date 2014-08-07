74f60f9bcda08dc904a7780d95221e07b27b40c7
santiago <sjcotto@gmail.com>
2014-08-07 10:40:26 -0300

    bin

--- SimpleDroidRest/src/com/github/sjcotto/simpledroidrest/test/RequestTest.java
+++ SimpleDroidRest/src/com/github/sjcotto/simpledroidrest/test/RequestTest.java
@@ -2,11 +2,14 @@ package com.github.sjcotto.simpledroidrest.test;
 
 import junit.framework.TestCase;
 
+import org.json.JSONObject;
 import org.junit.Test;
 
+import com.github.sjcotto.simpledroidrest.HTTPMethod;
 import com.github.sjcotto.simpledroidrest.DroidRequest;
 import com.github.sjcotto.simpledroidrest.DroidResponse;
-import com.github.sjcotto.simpledroidrest.HTTPMethod;
+
+import android.widget.Toast;
 
 public class RequestTest extends TestCase {
 
@@ -33,63 +36,94 @@ public class RequestTest extends TestCase {
 
 		Thread.sleep(5 * 1000);
 	}
-
+	
 	/*
-	 * @Test public void testPost() throws Exception {
-	 * 
-	 * final JSONObject json = new JSONObject(); json.put("name", "kona");
-	 * 
-	 * DroidRequest request = new DroidRequest() { { this.url =
-	 * "http://app.konacloud.io/api/taio/hello/r_api"; this.method =
-	 * HTTPMethod.POST; this.data = json.toString(); this.accessToken =
-	 * "5b7fb5bd-addc-4d72-8fb3-3e2b90fcbf69"; }
-	 * 
-	 * @Override public void onSuccess(String jsonObject) {
-	 * assertNotNull(jsonObject); }
-	 * 
-	 * @Override public void onFailure(DroidResponse res) { assertEquals(false,
-	 * true);
-	 * 
-	 * } }; request.make();
-	 * 
-	 * Thread.sleep(5 * 1000); }
-	 * 
-	 * @Test public void testPut() throws Exception {
-	 * 
-	 * final JSONObject json = new JSONObject(); json.put("name", "kona");
-	 * 
-	 * DroidRequest request = new DroidRequest() { { this.url =
-	 * "http://app.konacloud.io/api/taio/hello/r_api"; this.method =
-	 * HTTPMethod.PUT; this.data = json.toString(); this.accessToken =
-	 * "5b7fb5bd-addc-4d72-8fb3-3e2b90fcbf69"; }
-	 * 
-	 * @Override public void onSuccess(String jsonObject) {
-	 * assertNotNull(jsonObject); }
-	 * 
-	 * @Override public void onFailure(DroidResponse res) { assertEquals(false,
-	 * true);
-	 * 
-	 * } }; request.make();
-	 * 
-	 * Thread.sleep(5 * 1000);
-	 * 
-	 * }
-	 * 
-	 * @Test public void testDelete() throws Exception {
-	 * 
-	 * DroidRequest request = new DroidRequest() { { this.url =
-	 * "http://app.konacloud.io/api/taio/hello/r_api"; this.method =
-	 * HTTPMethod.DELETE; this.accessToken =
-	 * "5b7fb5bd-addc-4d72-8fb3-3e2b90fcbf69"; }
-	 * 
-	 * @Override public void onSuccess(String jsonObject) {
-	 * assertNotNull(jsonObject); }
-	 * 
-	 * @Override public void onFailure(DroidResponse res) { assertEquals(false,
-	 * true);
-	 * 
-	 * } }; request.make();
-	 * 
-	 * Thread.sleep(5 * 1000); }
-	 */
+	@Test
+	public void testPost() throws Exception {
+
+		final JSONObject json = new JSONObject();
+		json.put("name", "kona");
+
+		DroidRequest request = new DroidRequest() {
+			{
+				this.url = "http://app.konacloud.io/api/taio/hello/r_api";
+				this.method = HTTPMethod.POST;
+				this.data = json.toString();
+				this.accessToken = "5b7fb5bd-addc-4d72-8fb3-3e2b90fcbf69";
+			}
+
+			@Override
+			public void onSuccess(String jsonObject) {
+				assertNotNull(jsonObject);
+			}
+
+			@Override
+			public void onFailure(DroidResponse res) {
+				assertEquals(false, true);
+
+			}
+		};
+		request.make();
+
+		Thread.sleep(5 * 1000);
+	}
+
+	@Test
+	public void testPut() throws Exception {
+
+		final JSONObject json = new JSONObject();
+		json.put("name", "kona");
+
+		DroidRequest request = new DroidRequest() {
+			{
+				this.url = "http://app.konacloud.io/api/taio/hello/r_api";
+				this.method = HTTPMethod.PUT;
+				this.data = json.toString();
+				this.accessToken = "5b7fb5bd-addc-4d72-8fb3-3e2b90fcbf69";
+			}
+
+			@Override
+			public void onSuccess(String jsonObject) {
+				assertNotNull(jsonObject);
+			}
+
+			@Override
+			public void onFailure(DroidResponse res) {
+				assertEquals(false, true);
+
+			}
+		};
+		request.make();
+
+		Thread.sleep(5 * 1000);
+
+	}
+
+	@Test
+	public void testDelete() throws Exception {
+
+		DroidRequest request = new DroidRequest() {
+			{
+				this.url = "http://app.konacloud.io/api/taio/hello/r_api";
+				this.method = HTTPMethod.DELETE;
+				this.accessToken = "5b7fb5bd-addc-4d72-8fb3-3e2b90fcbf69";
+			}
+
+			@Override
+			public void onSuccess(String jsonObject) {
+				assertNotNull(jsonObject);
+			}
+
+			@Override
+			public void onFailure(DroidResponse res) {
+				assertEquals(false, true);
+
+			}
+		};
+		request.make();
+
+		Thread.sleep(5 * 1000);
+	}
+	
+	*/
 }
